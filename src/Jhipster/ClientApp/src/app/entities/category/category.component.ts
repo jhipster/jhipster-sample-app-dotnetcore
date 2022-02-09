36acc24c60540a81042a3a884e864826bb3ef600
eisnerw <eisnerw@hotmail.com>
2022-02-09 15:32:04 -0500

    Use common rulesetMap when showing

--- src/Jhipster/ClientApp/src/app/entities/category/category.component.ts
+++ src/Jhipster/ClientApp/src/app/entities/category/category.component.ts
@@ -215,12 +215,11 @@ export class CategoryComponent implements OnInit, OnDestroy {
 
   showSearchDialog(queryBuilder : any) : void {
     let rulesets : IStoredRuleset[] = [];
-    const rulesetMap = new Map<string, IQuery | IQueryRule>();
     this.rulesetService.query().pipe(map((res: any): void=> {
       rulesets = res.body || [];
       rulesets?.forEach(r=>{
         const query : IQuery = JSON.parse(r.jsonString as string) as IQuery;
-        rulesetMap.set(r.name as string, this.birthdayQueryParserService.normalize(query));
+        this.rulesetMap.set(r.name as string, this.birthdayQueryParserService.normalize(query));
       }); 
       let queryObject : any = this.birthdayQueryParserService.parse(this.searchQueryAsString);
       if (queryObject.Invalid){
